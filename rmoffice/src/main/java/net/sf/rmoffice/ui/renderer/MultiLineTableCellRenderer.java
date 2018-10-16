079df5fa5af7d8af500eb8465fae3988efdf6ae0
Daniel Nettesheim <daniel@nettesheim.name>
2018-10-16 18:35:05 +0200

    update last name of author

--- rmoffice/src/main/java/net/sf/rmoffice/ui/renderer/MultiLineTableCellRenderer.java
+++ rmoffice/src/main/java/net/sf/rmoffice/ui/renderer/MultiLineTableCellRenderer.java
@@ -1,6 +1,6 @@
 package net.sf.rmoffice.ui.renderer;
 /*
- * Copyright 2012 Daniel Golesny
+ * Copyright 2012 Daniel Nettesheim
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
